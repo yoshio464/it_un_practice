13320c421595586d31c8686f9c762df25501c298
yoshio464 <67165486+yoshio464@users.noreply.github.com>
2020-06-25 05:18:56 +0900

    initial commit
    練習

---
+++ html practice/js/main.js
@@ -0,0 +1,26 @@
+// 'use strict';
+
+// const button = document.getElementById('addBtn');
+// const lists = document.getElementById('lists');
+
+// async function getUser() {
+//   const res = await fetch('https://jsonplaceholder.typicode.com/users');
+//   const users = await res.json();
+//   return users;
+// }
+
+// async function listUsers() {
+//   const users = await getUser();
+//   users.forEach(addList);
+// };
+
+// function addList(user)  {
+//   const list = document.createElement('li');
+//   list.innerText = user.name;
+//   lists.appendChild(list);
+// }
+
+
+// window.addEventListener('load', listUsers);
+// button.addEventListener('click', listUsers);
+
